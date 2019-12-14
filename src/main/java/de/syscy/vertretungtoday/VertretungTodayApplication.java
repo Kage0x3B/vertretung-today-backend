1652d101dd7c8b5fffa51f50e2999bf21483298f
Kage0x3B <kage0x3b@outlook.com>
2019-12-14 19:36:27 +0100

    Initial code commit

---
+++ src/main/java/de/syscy/vertretungtoday/VertretungTodayApplication.java
@@ -0,0 +1,13 @@
+package de.syscy.vertretungtoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertretungTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertretungTodayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/syscy/vertretungtoday/VertretungTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package de.syscy.vertretungtoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertretungTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
